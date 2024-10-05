7cf6d54f53e7d7ddde5957608c031412c97f0e46
AlwanBanu17 <alwanbanu@gmail.com>
2024-10-05 15:25:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+nama_lengkap = "Alwan Banu"
+nim = 0
+prodi = "Teknik Informatika"
+nilai = 8.5
+lulus_ujian = True
+
+print(f"nama: {nama_lengkap} \nNim : {nim} \nprodi: {prodi} \nnilai: {nilai} \nstatus ujian: {lulus_ujian}")
+
+
